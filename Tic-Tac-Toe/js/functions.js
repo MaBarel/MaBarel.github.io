b1deca24257bc3f60c8aff72e5d21b62953ec4df
BenDoverMarij <115532706+MaBarel@users.noreply.github.com>
2023-04-11 11:39:21 +0200

    previous one was wrong so this one now is the right one

--- Tic-Tac-Toe/js/functions.js
+++ Tic-Tac-Toe/js/functions.js
@@ -1,1 +1,275 @@
 console.log("functions loaded");
+function startGame2() {//this function starts pve mode
+    blockElement.forEach(block => {
+        block.classList.remove("highlight");
+        playerWin = false;
+        botWin = false;
+        block.classList.remove("blocko");
+        block.classList.remove("blockx");
+        block.removeEventListener("click", clickPressed2);
+        counter = 0;
+        winLock = 0;
+        block.addEventListener('click', clickPressed2, { once: true });
+    });
+    winningMessage2.classList.remove("show");
+    pointBox1.innerHTML = "Player: " + scoreBoardP;
+    pointBox2.innerHTML = "Bot:    " + scoreBoardB;
+}
+function startGame() { //this function starts the game in pvp mode
+    blockElement.forEach(block => {
+        block.classList.remove("highlight");
+        oWin = false;
+        xWin = false;
+        block.classList.remove("blocko");
+        block.classList.remove("blockx");
+        block.removeEventListener("click", clickPressed);
+        counter = 0
+        winLock = 0
+        block.addEventListener('click', clickPressed, { once: true });
+    });
+    winningMessage.classList.remove("show");
+    pointBox1.innerHTML = "X points " + parseInt(scoreBoardX);
+    pointBox2.innerHTML = "O points " + parseInt(scoreBoardO);
+}
+//this is each individual block click for pvp
+function clickPressed(e) {
+    const block = e.target;
+    const XorO = turn ? O_Class : X_Class;
+    console.log("clicked");
+    placeMark(block, XorO);
+    checkWinner();
+    DrawCheck();
+    flip();
+}
+//this is each individual block click for pve
+function clickPressed2(e) {
+    const block = e.target;
+    const XorO = X_Class;
+    console.log("clicked");
+    placeMark(block, XorO);
+    checkWinner2();
+    DrawCheck2();
+    bot();
+}
+//this function adds the X/O
+function placeMark(block, XorO) {
+    block.classList.add(XorO)
+}
+//this function switches the X/O
+function flip() {
+    turn = !turn
+}
+//this fucntion checks for winners using a array
+function checkWinner() {
+    for (let i = 0; i < winningNumbas.length; i++) {
+        const winnings = winningNumbas[i];
+        const no1 = winnings[0];
+        const no2 = winnings[1];
+        const no3 = winnings[2];
+
+        const check1 = blockElement[no1];
+        const check2 = blockElement[no2];
+        const check3 = blockElement[no3];
+
+        //check that check1, check2 and check3 have the sameclasses
+        if (check1.classList.contains(X_Class) && check2.classList.contains(X_Class) && check3.classList.contains(X_Class)) {
+            winningText.innerHTML = localStorage.getItem("playerone") + " wins";
+            winningMessage.classList.add("show");
+            xWin = true;
+            winLock = 1;
+            check1.classList.add("highlight")
+            check2.classList.add("highlight")
+            check3.classList.add("highlight")
+        } else if (check1.classList.contains(O_Class) && check2.classList.contains(O_Class) && check3.classList.contains(O_Class)) {
+            winningText.innerHTML = localStorage.getItem("playertwo") + " wins";
+            winningMessage.classList.add("show")
+            oWin = true;
+            winLock = 1;
+            check1.classList.add("highlight")
+            check2.classList.add("highlight")
+            check3.classList.add("highlight")
+        }
+    }
+    console.log(oWin)
+    if (oWin === true) {
+        scoreBoardO = scoreBoardO + 1;
+        localStorage.setItem("Opoints", scoreBoardO)
+
+        if (isNaN(parseInt(scoreBoardX))) {
+            scoreBoardX = 0;
+        };
+        pointBox2.innerHTML = "O points " + scoreBoardO;
+        pointBox1.innerHTML = "X points " + scoreBoardX;
+    } else if (xWin === true) {
+        scoreBoardX = scoreBoardX + 1;
+        localStorage.setItem("Xpoints", scoreBoardX);
+
+        if (isNaN(scoreBoardO)) {
+            scoreBoardO = 0;
+        };
+        pointBox1.innerHTML = "X points " + scoreBoardX;
+        pointBox2.innerHTML = "O points " + scoreBoardO;
+    }
+}
+//this is the winning check for the pve version
+function checkWinner2() {
+    for (let i = 0; i < winningNumbas.length; i++) {
+        const winnings = winningNumbas[i];
+        const no1 = winnings[0];
+        const no2 = winnings[1];
+        const no3 = winnings[2];
+
+        const check1 = blockElement[no1];
+        const check2 = blockElement[no2];
+        const check3 = blockElement[no3];
+        console.log("checked")
+        //check that check1, check2 and check3 have the sameclasses
+        if (check1.classList.contains(X_Class) && check2.classList.contains(X_Class) && check3.classList.contains(X_Class)) {
+            winningText2.innerHTML = "You Win";
+            winningMessage2.classList.add("show");
+            playerWin = true;
+            winLock = 1;
+            check1.classList.add("highlight");
+            check2.classList.add("highlight");
+            check3.classList.add("highlight");
+        }
+    }
+    if (playerWin === true) {
+        scoreBoardP = scoreBoardP + 1;
+        localStorage.setItem("playerpoints", scoreBoardP);
+        if (isNaN(parseInt(scoreBoardB))) { scoreBoardB = 0; };
+        pointBox1.innerHTML = "Player: " + scoreBoardP;
+        pointBox2.innerHTML = "Bot: ", + scoreBoardB;
+    }
+}
+//this is the bot function 
+function bot() {
+    for (let i = 0; i < winningNumbas.length; i++) {
+        const winnings = winningNumbas[i];
+        const no1 = winnings[0];
+        const no2 = winnings[1];
+        const no3 = winnings[2];
+
+        const check1 = blockElement[no1];
+        const check2 = blockElement[no2];
+        const check3 = blockElement[no3];
+
+        if (check3.classList.contains(O_Class) && check2.classList.contains(O_Class) && lock === false &&
+            !check1.classList.contains(X_Class) && !check1.classList.contains(O_Class)) {
+            check1.classList.add(O_Class);
+            check1.removeEventListener("click", clickPressed2);
+            lock = true;
+        } else if (check1.classList.contains(O_Class) && check3.classList.contains(O_Class) && lock === false &&
+            !check2.classList.contains(X_Class) && !check2.classList.contains(O_Class)) {
+            check2.classList.add(O_Class);
+            check2.removeEventListener("click", clickPressed2);
+            lock = true;
+        } else if (check1.classList.contains(O_Class) && check2.classList.contains(O_Class) && lock === false &&
+            !check3.classList.contains(X_Class) && !check3.classList.contains(O_Class)) {
+            check3.classList.add(O_Class);
+            check3.removeEventListener("click", clickPressed2);
+            lock = true;
+        } 
+    }
+    for (let i = 0; i < winningNumbas.length; i++) {
+        const winnings = winningNumbas[i];
+        const no1 = winnings[0];
+        const no2 = winnings[1];
+        const no3 = winnings[2];
+
+        const check1 = blockElement[no1];
+        const check2 = blockElement[no2];
+        const check3 = blockElement[no3];
+
+        if (check3.classList.contains(X_Class) && check2.classList.contains(X_Class) && lock === false &&
+            !check1.classList.contains(X_Class) && !check1.classList.contains(O_Class)) {
+            check1.classList.add(O_Class);
+            check1.removeEventListener("click", clickPressed2);
+            lock = true;
+        } else if (check1.classList.contains(X_Class) && check3.classList.contains(X_Class) && lock === false &&
+            !check2.classList.contains(X_Class) && !check2.classList.contains(O_Class)) {
+            check2.classList.add(O_Class);
+            check2.removeEventListener("click", clickPressed2);
+            lock = true;
+        } else if (check1.classList.contains(X_Class) && check2.classList.contains(X_Class) && lock === false &&
+            !check3.classList.contains(X_Class) && !check3.classList.contains(O_Class)) {
+            check3.classList.add(O_Class);
+            check3.removeEventListener("click", clickPressed2);
+            lock = true;
+        }
+
+        if (check1.classList.contains(O_Class) && check2.classList.contains(O_Class) && check3.classList.contains(O_Class)) {
+            winningText2.innerHTML = "Computer" + " wins";
+            winningMessage2.classList.add("show")
+            botWin = true;
+            winLock = 1;
+            check1.classList.add("highlight");
+            check2.classList.add("highlight");
+            check3.classList.add("highlight");
+        }
+        console.log(blockElement)
+    }
+    if (lock === false) {
+        randombot();
+        lock = true
+    }
+    lock = false;
+    console.log(lock)
+    if (botWin === true) {
+        scoreBoardB = scoreBoardB + 1;
+        localStorage.setItem("botpoints", scoreBoardB);
+        if (isNaN(parseInt(scoreBoardP))) { scoreBoardP = 0; };
+        pointBox1.innerHTML = "Player: " + scoreBoardP;
+        pointBox2.innerHTML = "Bot: " + scoreBoardB;
+    }
+}
+//overige buttons
+function pressed() {
+    startGame();
+}
+function triggerd() {
+    startGame2();
+}
+function started() {
+    startGame();
+    startscreen.classList.remove("show");
+}
+function started2() {
+    startGame2();
+    startscreen.classList.remove("show");
+}
+function saved() {
+    localStorage.setItem('playerone', playerOne.value);
+    localStorage.setItem('playertwo', playerTwo.value);
+}
+function removed() {
+    localStorage.clear();
+    location.reload();
+}
+function DrawCheck() {
+    counter = counter + 1;
+    console.log(winLock)
+    if (counter === 9 && winLock === 0) { // if all 9 have been pressed it counts up
+        winningText.innerHTML = "Draw";
+        winningMessage.classList.add("show")
+        console.log(winLock)
+    }
+}
+function DrawCheck2() {
+    counter = counter + 1;
+    console.log(winLock)
+    if (counter === 5 && winLock === 0) { // if all 5 have been pressed it counts up
+        winningText2.innerHTML = "Draw";
+        winningMessage2.classList.add("show")
+        console.log(winLock)
+    }
+}
+function randombot() {
+    let num = Math.floor(Math.random() * 8);
+    if (!blockElement[num].classList.contains(X_Class) && !blockElement[num].classList.contains(O_Class)) {
+        blockElement[num].classList.add(O_Class);
+        blockElement[num].removeEventListener("click", clickPressed2);
+    } else if (blockElement[num].classList.contains(X_Class) || blockElement[num].classList.contains(O_Class)) {
+        randombot();
+    }
+}
--- Tic-Tac-Toe/js/main.js
+++ Tic-Tac-Toe/js/main.js
@@ -1,1 +1,81 @@
 console.log("main loaded");
+//variables
+const X_Class = "blockx";
+const O_Class = "blocko";
+const winningNumbas = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const winningText = document.querySelector(".winningText");
+const winningText2 = document.querySelector(".winningTexttwo")
+const winningMessage = document.querySelector(".winningMessage")
+const winningMessage2 = document.querySelector("#winningpve")
+const blockElement = document.querySelectorAll('.block');
+const restartButton = document.querySelector(".winningMessagebutton")
+const restartButton2 = document.querySelector(".winningMessagebuttontwo")
+const playerOne = document.querySelector(".player1");
+const playerTwo = document.querySelector(".player2");
+const pointBox1 = document.querySelector(".pointx");
+const pointBox2 = document.querySelector(".pointo");
+const saveButton = document.querySelector(".savebutton");
+const removeSaveButton = document.querySelector(".removesavebutton");
+const startpvp = document.querySelector(".startpvp");
+const startpve = document.querySelector(".startpve");
+const startscreen = document.querySelector("#startmessage");
+let Xhistory = localStorage.getItem('Xpoints');
+let Ohistory = localStorage.getItem('Opoints');
+let playerHistory = localStorage.getItem("playerpoints");
+let botHistory = localStorage.getItem("botpoints");
+let counter = 0;
+let winLock = 0;
+let scoreBoardO = 0;
+let scoreBoardX = 0;
+let scoreBoardP = 0;
+let scoreBoardB = 0;
+let turn;
+let oWin = false;
+let xWin = false;
+let botWin = false;
+let playerWin = false;
+let lock = false;
+let rdmBotPacement = true;
+if (localStorage.getItem("playerone") === null) {
+    localStorage.setItem("playerone", "Player X")
+}
+if (localStorage.getItem("playertwo") === null) {
+    localStorage.setItem("playertwo", "Player O")
+}
+if (Ohistory === null) {
+    Ohistory = 0;
+}
+if (Xhistory === null) {
+    Xhistory = 0;
+}
+if (playerHistory === null) {
+    playerHistory = 0;
+}
+if (botHistory === null) {
+    botHistory = 0;
+}
+
+scoreBoardX = parseInt(Xhistory);
+scoreBoardO = parseInt(Ohistory);
+scoreBoardP = parseInt(playerHistory);
+scoreBoardB = parseInt(botHistory);
+//buttons and also start game function
+startscreen.classList.add("show");
+startpvp.addEventListener("click", started);
+startpve.addEventListener("click", started2);
+saveButton.addEventListener("click", saved);
+removeSaveButton.addEventListener("click", removed);
+restartButton.addEventListener("click", pressed);
+restartButton2.addEventListener("click", triggerd);
+console.log(localStorage.getItem("Xpoints"));
+
+
